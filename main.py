ce149809dc90b85c5b7c8eae11c8eb78b4cb0ae9
Munim <iam@munim.net>
2019-08-25 23:52:06 +0600

    Moved API to ipdata.co

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAct
 
 LOGGER = logging.getLogger(__name__)
 
-ipgeolocation_api_url = 'https://api.ipgeolocation.io/ipgeo'
+ipdata_api_url = 'https://api.ipdata.co/'
 ip_regex = r"((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))"
 
 
@@ -28,26 +28,34 @@ class IPLookupExtension(Extension):
     
     def lookup(self, ip = None):
         params = {
-            "apiKey": self.preferences['ipl_api_key'],
+            "api-key": self.preferences['ipl_api_key'],
         }
 
+        url = ipdata_api_url
+
         if ip != "?":
-            params["ip"] = ip
+            url = "%s%s" % (ipdata_api_url, ip)
         
-        r = requests.get(ipgeolocation_api_url, params = params)
+        r = requests.get(url, params = params, timeout = 3)
         
         if r.status_code == 200:
             response = r.json()
 
             out_arr = {}
 
-            out_arr["IP"] = response.ip
-            out_arr["Location"] = "%s, %s (%s)" % (response.city, response.country_name, response.country_code2)
-            out_arr["Coordiates"] = "%s, %s" % (response.latitude, response.longitude)
-            out_arr["Calling code"] = response.calling_code
-            out_arr["Languages"] = response.languages
-            out_arr["ISP"] = response.isp
-            out_arr["Currency"] = "%s (%s)" % (response.currency.symbol, response.currency.code)
+            print(response)
+
+            out_arr["IP"] = response["ip"]
+            location = [response["city"], response["country_name"]]
+
+            location = ", ".join(filter(None, location))
+            location = location + " (%s)" % response["country_code"] if response["country_code"] != None else ""
+            out_arr["Location"] = location
+            out_arr["Coordiates"] = "%s, %s" % (response["latitude"], response["longitude"])
+            out_arr["Calling code"] = response["calling_code"]
+            out_arr["Languages"] = ", ".join(obj["name"] for obj in response["languages"])
+            out_arr["ISP"] = response["organisation"]
+            out_arr["Currency"] = "%s (%s)" % (response["currency"]["name"], response["currency"]["code"])
 
             return out_arr
         else:
@@ -98,7 +106,7 @@ class KeywordQueryEventListener(EventListener):
 
 
 class LookupException(Exception):
-    """ Exception thrown when there was an error calling the conversion API """
+    """ Exception thrown when there was an error calling the lookup API """
     pass
 
 
